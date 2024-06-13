a9f55df8ec5fe7b13f0a3a2a0bf12288b224168d
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-06-13 10:42:34 +0530

    Time: 3 ms (62.42%), Space: 44.3 MB (14.32%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
@@ -2,11 +2,11 @@ class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
         Arrays.sort(seats);
         Arrays.sort(students);
-        int i = 0,j = 0;
+        int i = 0;
         int ans = 0;
-        while(i<seats.length && j<students.length){
-            ans+=Math.abs(seats[i]-students[j]);
-            i++;j++;
+        while(i<seats.length){
+            ans+=Math.abs(seats[i]-students[i]);
+            i++;
         }
         return ans;
     }
