d9aef1582aab2dceaee29c36a2100ce8c9872358
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-06-14 09:50:10 +0530

    Time: 4 ms (100%), Space: 60.5 MB (20%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -1,14 +1,23 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        Arrays.sort(nums);
-        int ans = 0;
-        int curr = 0;
-        for(int i = 0;i<nums.length;i++){
-            curr = Math.max(curr,nums[i]);
-            ans+=(curr - nums[i]);
-            curr++;
+        int max = 0;
+        int n = nums.length;
+        for(int num : nums){
+            max = Math.max(max,num);
         }
-        return ans;
+       int[] freq = new int[n+max];
+       for(int num : nums){
+        freq[num]++;
+       }
+       int ans = 0;
+       for(int i = 0;i<freq.length;i++){
+            if(freq[i]<=1)continue;
+            int mov = freq[i]-1;
+            freq[i+1] += mov;
+            freq[i] = 1;
+            ans+=mov;
 
+       }
+       return ans;
     }
 }
