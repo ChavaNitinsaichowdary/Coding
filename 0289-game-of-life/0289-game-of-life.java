f87dd4dc3782803c1f86457bd65b8a80e6996697
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-07-26 19:34:54 +0530

    Time: 0 ms (100%), Space: 41.8 MB (22.45%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -2,33 +2,41 @@ class Solution {
     public void gameOfLife(int[][] board) {
         int rows = board.length;
         int cols = board[0].length;
-        int[][] neighborCount = new int[rows][cols];
+        int[] x = {0,1,1,-1,0,-1,1,-1};
+        int[] y = {1,0,1,-1,-1,0,-1,1};
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 1) {
-                    if (i > 0) neighborCount[i-1][j]++;
-                    if (j > 0) neighborCount[i][j-1]++;
-                    if (i > 0 && j > 0) neighborCount[i-1][j-1]++;
-                    if (i < rows - 1) neighborCount[i+1][j]++;
-                    if (j < cols - 1) neighborCount[i][j+1]++;
-                    if (i < rows - 1 && j < cols - 1) neighborCount[i+1][j+1]++;
-                    if (i > 0 && j < cols - 1) neighborCount[i-1][j+1]++;
-                    if (i < rows - 1 && j > 0) neighborCount[i+1][j-1]++;
+                    int live = 0;
+                    for(int k = 0;k<8;k++){
+                        int cx = i + x[k];
+                        int cy = j + y[k];
+                        if(cx>=0 && cy>=0 && cx<board.length && cy<board[0].length){
+                            if((board[cx][cy]&1) == 1) live++;
+                        }
+                    }
+                    if((board[i][j]&1)==1){
+                        if(live<2 || live>3){
+                            board[i][j] = board[i][j]&1;
+                        }
+                        else{
+                            board[i][j] = board[i][j]|2;
+                        }
+                    }
+                    else{
+                        if(live==3){
+                            board[i][j] = board[i][j]|2;
+                        }
+                    }
+                     
                 }
             }
-        }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 1) {
-                    if (neighborCount[i][j] < 2 || neighborCount[i][j] > 3) {
-                        board[i][j] = 0;
-                    }
-                } else {
-                    if (neighborCount[i][j] == 3) {
-                        board[i][j] = 1;
-                    }
-                }
+        
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
+                board[i][j] = board[i][j]>>1;
             }
         }
+
+        
     }
 }
