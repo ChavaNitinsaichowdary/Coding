7a9f49ab33c41e442e9518c1a2c475c7988e7b7f
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-08-12 21:32:44 +0530

    Time: 7 ms (34.55%), Space: 16.8 MB (14.24%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> sub;
-    void backtrack(int idx,vector<int>& freq,vector<int>& candidates,int target){
+    void helper(int idx,vector<int>& freq,int target){
         if(target==0){
             ans.push_back(sub);
             return;
         }
-        if(idx==freq.size() || target<0){
-            return;
-        }
-        backtrack(idx+1,freq,candidates,target);
+        if(target<0 || idx>50)return;
+        helper(idx+1,freq,target);
         if(freq[idx]){
             freq[idx]--;
             sub.push_back(idx);
-            backtrack(idx,freq,candidates,target-idx);
+            helper(idx,freq,target-idx);
             sub.pop_back();
             freq[idx]++;
         }
+        
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<int> freq(51);
-        for(auto &i : candidates){
+        for(int &i : candidates){
             freq[i]++;
         }
-        backtrack(1,freq,candidates,target);
+        helper(1,freq,target);
         return ans;
-
+        
     }
 };
