196c3f761970fbfbf7fef3d9528d66f453e6791b
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-08-26 21:02:05 +0530

    Time: 9 ms (86.24%), Space: 12.9 MB (84.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    int helper(vector<int> &nums,int index,int prev,vector<vector<int>>& dp){
-        if(index>=nums.size())return 0;
-        if(dp[index][prev+1]!=-1)return dp[index][prev+1];
-        if(prev==-1){
-            return dp[index][prev+1]= max(1+helper(nums,index+1,index,dp),helper(nums,index+1,prev,dp));
-        }
-        if(nums[index]>nums[prev]){
-            return dp[index][prev+1] = max(1+helper(nums,index+1,index,dp),helper(nums,index+1,prev,dp));
-        }
-        else{
-            return dp[index][prev+1] = helper(nums,index+1,prev,dp);
-        }
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-       vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return helper(nums,0,-1,dp);
+        vector<int> v;
+        for(int i = 0;i<nums.size();i++){
+           auto it  = std::lower_bound(v.begin(),v.end(),nums[i]);
+           if(it==v.end()) v.push_back(nums[i]);
+           else *it = nums[i];
+        }
+        return v.size();
     }
 };
