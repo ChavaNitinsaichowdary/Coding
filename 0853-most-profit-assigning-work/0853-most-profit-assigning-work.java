4b1b41e82375f60d9716ba96bb9c2d849868d7e5
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-08-27 14:35:58 +0530

    Time: 20 ms (61.46%), Space: 45.7 MB (65.28%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -1,34 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        
-        List<int[]> jobs = new ArrayList<>();
-        for (int i = 0; i < difficulty.length; i++) {
-            jobs.add(new int[]{difficulty[i], profit[i]});
+        List<int[]> ls = new ArrayList<>();
+        for(int i = 0;i<difficulty.length;i++){
+            ls.add(new int[]{difficulty[i],profit[i]});
         }
-        
-        
-        jobs.sort((a, b) -> a[0] - b[0]);
-        
-       
+        Collections.sort(ls,(a,b)->a[0]-b[0]);
         Arrays.sort(worker);
-        
         int ans = 0;
-        int maxProfit = 0;
-        int jobIndex = 0;
-        
-        // Iterate through each worker
-        for (int ability : worker) {
-            // Update the maximum profit for the jobs the worker can handle
-            while (jobIndex < jobs.size() && jobs.get(jobIndex)[0] <= ability) {
-                maxProfit = Math.max(maxProfit, jobs.get(jobIndex)[1]);
-                jobIndex++;
+        int max = 0;
+        int j = 0;
+        for(int i = 0;i<worker.length;i++){
+            while(j<ls.size() && ls.get(j)[0]<=worker[i]){
+                max = Math.max(max,ls.get(j)[1]);
+                j++;
             }
-            // Add the maximum profit that the worker can achieve
-            ans += maxProfit;
+            ans+=max;
         }
-        
         return ans;
     }
 }
