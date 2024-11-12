ce3a82e1a89790b60a9d537a9bc84ef8fc37b1c1
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2024-11-12 21:57:23 +0530

    Time: 7 ms (5.94%), Space: 18.6 MB (8.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    static void helper(int index,vector<vector<int>> &ans,vector<int>& sub,vector<int>& nums){
+    vector<vector<int>> helper(vector<int>& nums,vector<int> v,int index){
         if(index==nums.size()){
-            ans.push_back(sub);
-            return;
+            vector<vector<int>> sub;
+            sub.push_back(v);
+            return sub;
         }
-            helper(index+1,ans,sub,nums);
-            sub.push_back(nums[index]);
-            helper(index+1,ans,sub,nums);
-            sub.pop_back();
-
+        vector<vector<int>> ans;
+        vector<vector<int>> ns = helper(nums,v,index+1);
+        v.push_back(nums[index]);
+        vector<vector<int>> s = helper(nums,v,index+1);
+        v.pop_back();
+        for(vector<int> t : ns){
+            ans.push_back(t);
+        }
+        for(vector<int> t : s){
+            ans.push_back(t);
+        }
+        return ans;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> sub;
-        helper(0,ans,sub,nums);
-        return ans;
+        vector<int> v;
+        return helper(nums,v,0);
     }
 };
